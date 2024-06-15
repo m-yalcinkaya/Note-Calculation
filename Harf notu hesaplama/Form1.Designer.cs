42b66011ab3aa8d9e97cfa337c4099e3b91e07d6
Mustafa Yalçınkaya <myalcinkaya136@gmail.com>
2024-06-15 16:39:12 +0300

    first

---
+++ Harf notu hesaplama/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Harf_notu_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 45);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 38);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 111);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(293, 38);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ders Adı: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "AKTS:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Harf notu:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 44);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Göster";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(500, 45);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(454, 384);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ders Adı";
+            this.columnHeader1.Width = 139;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "AKTS";
+            this.columnHeader2.Width = 86;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Harf Notu";
+            this.columnHeader3.Width = 111;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AA",
+            "BA",
+            "BB",
+            "CB",
+            "CC",
+            "DC",
+            "DD",
+            "FD",
+            "FF"});
+            this.comboBox1.Location = new System.Drawing.Point(155, 174);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(114, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(384, 467);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 44);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Hesapla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(51, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 44);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "id";
+            this.columnHeader4.Width = 67;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button5.Enabled = false;
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(65, 443);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(277, 92);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Sonuc: ";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(230, 337);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 44);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Güncelle";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 609);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Harf notu hesaplama/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Harf_notu_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connect = new SqlConnection("Data Source=DESKTOP-DDOVUAJ;Initial Catalog=Notes;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+
+        private void showDatas()
+        {
+            listView1.Items.Clear();
+            connect.Open();
+            SqlCommand cmd = new SqlCommand("select *from note", connect);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while(dr.Read())
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = dr["id"].ToString();
+                item.SubItems.Add(dr["coursename"].ToString());
+                item.SubItems.Add(dr["acts"].ToString());
+                item.SubItems.Add(dr["letternote"].ToString());
+                listView1.Items.Add(item);
+            }
+            connect.Close();
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            showDatas();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            SqlCommand cmd = new SqlCommand("insert into note (coursename, acts, letternote) values ('" + textBox1.Text.ToString() + "', '" + textBox2.Text.ToString() + "','" + comboBox1.Text.ToString() + "')", connect);
+            cmd.ExecuteNonQuery();
+            connect.Close();
+            showDatas();
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int id = 0;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            SqlCommand cmd = new SqlCommand("delete from note where id =("+id+") ", connect);
+            cmd.ExecuteNonQuery();
+            connect.Close();
+            showDatas();
+        }
+        
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            id = int.Parse(listView1.SelectedItems[0].SubItems[0].Text);
+            textBox1.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            textBox2.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            comboBox1.Text = listView1.SelectedItems[0].SubItems[3].Text;
+        }
+
+
+        double factor(string letters)
+        {
+            switch (letters)
+            {
+                case "AA": return 4.00;  break;
+                case "BA": return 3.50; break;
+                case "BB": return 3.00; break;
+                case "CB": return 2.50; break;
+                case "CC": return 2.00; break;
+                case "DC": return 1.50; break;
+                case "DD": return 1.00; break;
+                case "FD": return 0.50; break;
+                case "FF": return 0.00; break;
+                default: return 0.00;
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            SqlCommand cmd = new SqlCommand("select *from note", connect);
+            SqlDataReader dr = cmd.ExecuteReader();
+            List<int> akts = new List<int>();
+            List<string> letters = new List<string>();
+            string letterNote = null;
+            while (dr.Read())
+            {
+
+                int acts = int.Parse(dr["acts"].ToString());
+                letterNote = dr["letternote"].ToString();
+
+                akts.Add(acts);
+                letters.Add(letterNote);
+                
+            }
+            connect.Close();
+
+            int totalActs = 0;
+            double totalFactor = 0;
+            for (int i = 0; i < akts.Count; i++)
+            {
+                totalActs += akts[i];
+                totalFactor += (akts[i] * factor(letters[i]));
+            }
+
+            double mean = totalFactor / totalActs;
+
+            button5.Text = mean.ToString();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            SqlCommand cmd = new SqlCommand("update note set coursename='" +textBox1.Text.ToString() + "', acts='" +textBox2.Text.ToString() + "',letternote='" +comboBox1.Text.ToString() + "' where id=" + id+"", connect);
+            cmd.ExecuteNonQuery();
+            connect.Close();
+            showDatas();
+        }
+    }
+}
